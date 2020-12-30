efcf8d69ef968311523dc5374c798764dc98dc37
Connie Leung <cleung334@gmail.com>
2020-12-30 08:20:44 +0800

    feat: add nuxt config file
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
 <style>
 html {
   font-family:
-    'Source Sans Pro',
+    'Mulish',
     -apple-system,
     BlinkMacSystemFont,
     'Segoe UI',
@@ -15,6 +15,7 @@ html {
     'Helvetica Neue',
     Arial,
     sans-serif;
+  font-weight: 300;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -31,32 +32,16 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+h1, h2, h3, h4 {
+  font-family:
+    'Poppins',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-weight: 600;
 }
 </style>
