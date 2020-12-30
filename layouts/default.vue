587af5736d6223d377c1f6155138f2e7e6ef1b4b
Connie Leung <cleung334@gmail.com>
2020-12-30 08:42:21 +0800

    add navigation and about page
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
-    <Nuxt />
+    <div class="content">
+      <AppMenu />
+      <Nuxt />
+    </div>
   </div>
 </template>
+
+<script>
+import AppMenu from '@/components/AppMenu';
+export default {
+  components: {
+    AppMenu
+  }
+}
+</script>
 
 <style>
 html {
