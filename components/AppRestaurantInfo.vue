6c365f9c7cd43cdd97918c5a2a6a7d7065327c3b
Connie Leung <cleung334@gmail.com>
2021-01-01 16:36:03 +0800

    feat: show restaurants and their menu
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ components/AppRestaurantInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="restaurantinfo">
+    <div v-for="store of fooddata" :key="store.id">
+      <h2>{{ store.name }}</h2>
+      <p>Delivery Time {{ store.deliveryTime }}</p>
+      <p>Rating {{ store.rating }}</p>
+      <p v-if="store.freeDelivery" class="label"><span>Free Delivery</span></p>
+      <div class="row">
+        <div v-for="menuItem of store.menu" :key="menuItem.id" class="items"
+          :style="`background: url(${menuItem.img}) no-repeat center center`">
+          <div class="iteminfo">
+            <div>
+              <h4>{{ menuItem.item }}</h4>
+              <p>{{ getPrice(menuItem.price) }}</p>
+            </div>
+            <button class="ghost">View Item ></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['fooddata'])
+  },
+  methods: {
+    getPrice(price) {
+      return `$ ${price.toFixed(2)}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="container">
     <AppHeader />
+    <AppRestaurantInfo />
   </main>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHead';
+import AppRestaurantInfo from '@/components/AppRestaurantInfo';
 
 export default {
   components: {
